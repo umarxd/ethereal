e3388605e7ef24ca1b49ba39e7afc31d31314f64
umarxd <xqcowsavedmylife@gmail.com>
2024-05-06 00:21:48 +0300

    [add] header texts for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import { Icons } from "@/components/Icons";
+
 export default function Home() {
   return (
-    <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eum
-      repudiandae minima quidem maiores veritatis omnis nulla officiis qui iste
-      animi eaque rem optio ea, eos magnam vel voluptatem quae.
-    </>
+    <div className="rounded-md  flex flex-col justify-center items-center gap-12 sm:mt-10">
+      <h1 className="text-xl sm:text-2xl text-center font-bold">
+        <span className="text-teal-500  font-extrabold">Ethereal</span> offers
+        you a rich catalog of beautiful handmade stickers.
+      </h1>
+      <div className="grid grid-cols-2 sm:flex">
+        <Icons.stickerTree className="w-20 h-20" />
+        <Icons.stickerHamburger className="w-20 h-20" />
+        <Icons.stickerDoubleHelix className="w-20 h-20" />
+        <Icons.stickerIcecream className="w-20 h-20" />
+      </div>
+
+      <div className="">
+        Welcome to our sticker wonderland, where creativity knows no bounds!
+        Dive into our kaleidoscope of designs, from whimsical characters to
+        intricate patterns, each sticker telling its own unique story. Whether
+        you're decking out your laptop, water bottle, or notebook, our
+        high-quality stickers are here to accompany you on every adventure.
+        Explore our collection today and let your imagination run wild!
+      </div>
+    </div>
   );
 }
