e859d7273cd49ef692874b9b11cc2fe2339b4cf1
umarxd <xqcowsavedmylife@gmail.com>
2024-05-06 23:28:29 +0300

    [style] minor styling change for home and auth screens

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,24 +1,9 @@
 import SignIn from "@/components/SignIn";
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
 
 const page = () => {
   return (
-    <div className="absolute inset-0">
-      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
-        <Link
-          href="/"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "self-start -mt-20"
-          )}
-        >
-          Home
-        </Link>
-
-        <SignIn />
-      </div>
+    <div className="absolute inset-0 h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+      <SignIn />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="rounded-md text-center flex flex-col justify-center items-center gap-12 sm:gap-16 sm:mt-24">
-      <h1 className="text-xl tracking-tight sm:text-3xl font-semibold">
-        <span className="text-teal-500 font-bold">Ethereal</span> offers you a
-        rich catalog of beautiful handmade stickers.
-      </h1>
-      <div className="grid grid-cols-2 sm:flex">
-        <Icons.stickerTree className="w-20 h-20" />
-        <Icons.stickerHamburger className="w-20 h-20" />
-        <Icons.stickerDoubleHelix className="w-20 h-20" />
-        <Icons.stickerIcecream className="w-20 h-20" />
+    <div className="rounded-md text-center flex flex-col justify-center items-center gap-12  sm:mt-24">
+      <div className="flex flex-col justify-center items-center gap-6">
+        <h1 className="text-2xl tracking-tight sm:text-3xl font-semibold">
+          <span className="text-teal-500 font-bold">Ethereal</span> offers you a
+          rich catalog of beautiful handmade stickers.
+        </h1>
+        <div className="grid grid-cols-2 sm:flex">
+          <Icons.stickerTree className="w-20 h-20" />
+          <Icons.stickerHamburger className="w-20 h-20" />
+          <Icons.stickerDoubleHelix className="w-20 h-20" />
+          <Icons.stickerIcecream className="w-20 h-20" />
+        </div>
       </div>
-
       <div className="text-lg text-muted-foreground">
         Welcome to our sticker wonderland, where creativity knows no bounds!
         Dive into our kaleidoscope of designs, from whimsical characters to
@@ -30,31 +31,4 @@ export default function Home() {
       </Link>
     </div>
   );
-}
-
-{
-  /* 
- <div>
-      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Your marketplace for high-quality{" "}
-          <span className="text-blue-600">digital assets</span>.
-        </h1>
-        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-          Welcome to DigitalHippo. Every asset on our platform is verified by
-          our team to ensure our highest quality standards.
-        </p>
-        <div className="flex flex-col sm:flex-row gap-4 mt-6">
-          <Link href="/products" className={buttonVariants()}>
-            Browse Trending
-          </Link>
-          <Button variant="ghost">Our quality promise &rarr;</Button>
-        </div>
-      </div>
-
-      <section className="border-t border-gray-200 bg-gray-50">
-        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0"></div>
-      </section>
-    </div>
-*/
 }
