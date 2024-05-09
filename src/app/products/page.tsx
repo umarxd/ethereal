f2f53ed50b1c86c541b38eec7dce0049d8de375d
umarxd <xqcowsavedmylife@gmail.com>
2024-05-09 19:28:09 +0300

    [style] small styling tweak on products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ import stickerData from "../../util/PlaceholderStickersData";
 
 const Products = ({}) => {
   return (
-    <div className="grid  md:grid-cols-4 gap-y-4 gap-x-4">
+    <div className="container grid md:grid-cols-4 gap-y-4 gap-x-4">
       {stickerData.map((sticker, index) => (
         <StickerCard key={index} sticker={sticker} />
       ))}
